88860532a33b3dc9d9e33da0b090504b8688dbf9
Ferenc Forgacs <forgacsf@gmail.com>
2020-12-14 17:47:06 +0100

    Google auth basics

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,11 +2,26 @@ import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
 
 const options = {
+	site: process.env.NEXTAUTH_URL,
 	// Configure one or more authentication providers
-	providers: [],
+	providers: [
+		Providers.Google({
+			clientId: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+		}),
+		Providers.Facebook({
+			clientId: process.env.FACEBOOK_APP_ID,
+			clientSecret: process.env.FACEBOOK_APP_SECRET,
+		}),
+	],
 
 	// A database is optional, but required to persist accounts in a database
-	database: process.env.DATABASE_URL,
+	database: {
+		type: "mongodb",
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		url: process.env.DATABASE_URL,
+	},
 };
 
 export default (req, res) => NextAuth(req, res, options);
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,25 @@
+import { signIn, signOut, useSession } from "next-auth/client";
+
 export default function Home() {
+	const [session, loading] = useSession();
+
+	if (loading) {
+		return <p>Loading...</p>;
+	}
+
 	return (
 		<div>
 			<h1>Welcome to twis ✨</h1>
+			{session && session.user.email}
+			<div>
+				<p>
+					<button onClick={() => signIn("google")}>Sign in with Google</button>
+				</p>
+
+				<p>
+					<button onClick={() => signIn("facebook")}>Sign in with Facebook</button>
+				</p>
+			</div>
 		</div>
 	);
 }
