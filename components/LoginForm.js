7bc6a46ad4cacb88d620385bf4d27728eca53359
Ferenc Forgacs <forgacsf@gmail.com>
2020-12-16 18:06:58 +0100

    Login componenet

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,3 +1,95 @@
+import { signIn, useSession } from "next-auth/client";
+import { useState } from "react";
+import Image from "next/image";
+
 export default function LoginForm() {
-	return <div>Log in...</div>;
+	const [session, loading] = useSession();
+	const [loginEmailAddress, setLoginEmailAddress] = useState("");
+	const [emailLoginLoading, setEmailLoginLoading] = useState(false);
+	const [isValidEmail, setIsValidEmail] = useState(false);
+
+	/**
+	 * Loading state
+	 */
+	if (loading) {
+		return <p>Loading...</p>;
+	}
+
+	/**
+	 * Redirect logged in users to dashboard
+	 */
+	if (session) {
+		router.push("/dashboard");
+		return <></>;
+	}
+
+	/**
+	 * Handle login with email address
+	 * @param {object} e Form submit event object
+	 */
+	const signInWithEmail = (e) => {
+		e.preventDefault();
+		setEmailLoginLoading(true);
+		localStorage.setItem("verifyRequestEmail", loginEmailAddress);
+		signIn("email", { email: loginEmailAddress });
+	};
+
+	/**
+	 * Handle email input field changes
+	 * @param {string} email Email address string
+	 */
+	const handleEmailUpdate = (email) => {
+		setLoginEmailAddress(email);
+		// check email format
+		setIsValidEmail(/\S+@\S+\.\S+/.test(email));
+	};
+
+	return (
+		<div id="login-form">
+			<header>
+				<span>TWiS ✨</span>
+				<h1>Sign up or log in</h1>
+			</header>
+
+			<section>
+				<div id="login-form__social">
+					<button onClick={() => signIn("google", { callbackUrl: "/dashboard" })}>
+						<Image src="/images/icons/icon-google.svg" alt="Google logo" width={24} height={24} />
+						Continue with Google
+					</button>
+
+					<button onClick={() => signIn("facebook", { callbackUrl: "/dashboard" })}>
+						<Image src="/images/icons/icon-facebook.svg" alt="Facebook logo" width={24} height={24} />
+						Continue with Facebook
+					</button>
+				</div>
+				<p id="login-form__separator">or</p>
+				<div id="login-form__email">
+					<form
+						method="post"
+						onSubmit={(e) => {
+							signInWithEmail(e);
+						}}
+					>
+						<input type="email" value={loginEmailAddress} onChange={(e) => handleEmailUpdate(e.target.value)} placeholder="Your email address" required="required" />
+						<button type="submit" disabled={emailLoginLoading || !isValidEmail}>
+							{emailLoginLoading ? "Sending email..." : `Continue with email`}
+						</button>
+					</form>
+				</div>
+
+				<div id="login-form__legal">
+					By continuing with Google, Facebook, or email, you agree to our{" "}
+					<a href="/terms-of-service" target="_blank" rel="noopener noreferrer">
+						Terms of Service
+					</a>{" "}
+					and{" "}
+					<a href="/privacy-policy" target="_blank" rel="noopener noreferrer">
+						Privacy Policy
+					</a>
+					.
+				</div>
+			</section>
+		</div>
+	);
 }
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,10 @@
-import { signIn, useSession } from "next-auth/client";
-import { useState } from "react";
+import { useSession } from "next-auth/client";
 import { useRouter } from "next/router";
+import LoginForm from "../components/LoginForm";
 
 export default function Home() {
 	const router = useRouter();
-
-	const [session, loading] = useSession();
-	const [loginEmailAddress, setLoginEmailAddress] = useState("");
-	const [emailLoginLoading, setEmailLoginLoading] = useState(false);
-
-	/**
-	 * Handle login with email address
-	 * @param {object} e Form submit event object
-	 */
-	const signInWithEmail = (e) => {
-		e.preventDefault();
-		setEmailLoginLoading(true);
-		localStorage.setItem("verifyRequestEmail", loginEmailAddress);
-		signIn("email", { email: loginEmailAddress });
-	};
-
-	/**
-	 * Loading state
-	 */
-	if (loading) {
-		return <p>Loading...</p>;
-	}
-
+	const [session] = useSession();
 	/**
 	 * Redirect logged in users to dashboard
 	 */
@@ -38,30 +16,5 @@ export default function Home() {
 	/**
 	 * Display login form to users
 	 */
-	return (
-		<div>
-			<h1>Welcome to twis ✨</h1>
-			{session && session.user.email}
-			<div>
-				<form
-					method="post"
-					onSubmit={(e) => {
-						signInWithEmail(e);
-					}}
-				>
-					<input type="email" value={loginEmailAddress} onChange={(e) => setLoginEmailAddress(e.target.value)} placeholder="Your email address" required="required" />
-					<button type="submit" disabled={emailLoginLoading}>
-						{emailLoginLoading ? "Sending email..." : `Log in with email`}
-					</button>
-				</form>
-				<p>
-					<button onClick={() => signIn("google", { callbackUrl: "/dashboard" })}>Sign in with Google</button>
-				</p>
-
-				<p>
-					<button onClick={() => signIn("facebook", { callbackUrl: "/dashboard" })}>Sign in with Facebook</button>
-				</p>
-			</div>
-		</div>
-	);
+	return <LoginForm />;
 }
