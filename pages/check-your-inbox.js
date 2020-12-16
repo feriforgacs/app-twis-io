75fc6532fa101198561aec5a4a8437110a83943d
Ferenc Forgacs <forgacsf@gmail.com>
2020-12-16 18:19:43 +0100

    Email login instructions page

--- pages/check-your-inbox.js
+++ pages/check-your-inbox.js
@@ -1,6 +1,10 @@
+import { useSession } from "next-auth/client";
 import { useState, useEffect } from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function CheckYourInbox() {
+	const [session] = useSession();
 	const [verifyRequestEmail, setVerifyRequestEmail] = useState();
 
 	useEffect(() => {
@@ -9,9 +13,37 @@ export default function CheckYourInbox() {
 		}
 	});
 
+	/**
+	 * Redirect logged in users to dashboard
+	 */
+	if (session) {
+		router.push("/dashboard");
+		return <></>;
+	}
+
 	return (
-		<div>
-			<h1>We've just sent an email to {verifyRequestEmail}</h1>
+		<div id="email-login-information">
+			<h1>Check your email for login instructions</h1>
+			<p>
+				We’ve sent an email with a magic link to <strong>{verifyRequestEmail}</strong>. The link in the email expires shortly, so please use it soon.
+			</p>
+			<div id="email-login-information__providers">
+				<a href="https://mail.google.com/mail/u/0/" target="_blank" rel="noopener noreferrer">
+					<Image src="/images/icons/icon-gmail.svg" alt="Gmail icon" width={24} height={24} />
+					Open Gmail
+				</a>
+				<a href="https://outlook.live.com/mail/0/inbox" target="_blank" rel="noopener noreferrer">
+					<Image src="/images/icons/icon-outlook.svg" alt="Outlook icon" width={24} height={24} />
+					Open Outlook
+				</a>
+			</div>
+			<p>Can’t find your code? Make sure you check your spam folder!</p>
+			<p>
+				Having trouble loggin in?{" "}
+				<Link href="/contact-us">
+					<a>Contact Us!</a>
+				</Link>
+			</p>
 		</div>
 	);
 }
