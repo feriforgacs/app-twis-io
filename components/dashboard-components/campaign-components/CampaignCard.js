65abf19df5061e85a1e14f7a85ba9acefac2856f
Ferenc Forgacs <forgacsf@gmail.com>
2020-12-29 14:41:40 +0100

    Campaign card component UI

--- components/dashboard-components/campaign-components/CampaignCard.js
+++ components/dashboard-components/campaign-components/CampaignCard.js
@@ -1,10 +1,31 @@
+import { useState, useRef, useEffect } from "react";
 import { format } from "date-fns";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function CampaignCard({ id, name, type, status, participants, visibleFrom, visibleTo }) {
+	const [navigationVisible, toggleNavigationVisible] = useState(false);
+
+	const campaignCardNavigationRef = useRef(null);
+
+	useEffect(() => {
+		const handleClickOutSide = (event) => {
+			if (campaignCardNavigationRef.current && !campaignCardNavigationRef.current.contains(event.target)) {
+				if (navigationVisible) {
+					toggleNavigationVisible(false);
+				}
+			}
+		};
+
+		document.addEventListener("mousedown", handleClickOutSide);
+
+		return () => {
+			document.removeEventListener("mousedown", handleClickOutSide);
+		};
+	}, [campaignCardNavigationRef, navigationVisible]);
 	return (
 		<div className="campaign-card">
-			<div className="campaign-card__header">
+			<div className="campaign-card__header campaign-card__section">
 				<h4 className="campaign-card__title">
 					<Link href={`/campaign/edit/${id}`} title={`Edit ${name}`}>
 						<a>
@@ -13,14 +34,36 @@ export default function CampaignCard({ id, name, type, status, participants, vis
 						</a>
 					</Link>
 				</h4>
+
+				<button className="button button--card-navigation" onClick={() => toggleNavigationVisible(!navigationVisible)}>
+					{!navigationVisible && <span>&hellip;</span>}
+
+					{navigationVisible && (
+						<svg viewBox="0 0 20 20">
+							<path d="M11.414 10l6.293-6.293a.999.999 0 1 0-1.414-1.414L10 8.586 3.707 2.293a.999.999 0 1 0-1.414 1.414L8.586 10l-6.293 6.293a.999.999 0 1 0 1.414 1.414L10 11.414l6.293 6.293a.997.997 0 0 0 1.414 0 .999.999 0 0 0 0-1.414L11.414 10z"></path>
+						</svg>
+					)}
+				</button>
+
+				{navigationVisible && (
+					<div className="campaign-card__navigation campaign-card__navigation--dropdown" ref={campaignCardNavigationRef}>
+						<a href="/" target="_blank" rel="noopener noreferrer" className="button button--dropdown">
+							View campaign
+						</a>
+						<button className="button button--dropdown">Duplicate Campaign</button>
+						<button className="button button--dropdown color--error">Delete Campaign</button>
+					</div>
+				)}
 			</div>
-			<div className="campaign-card__body">
+			<div className="campaign-card__body campaign-card__section">
 				<div className="campaign-card__meta">
 					<span className="badge badge--info badge--campaign-type">{type}</span>
-					<span className={`campaign-status badge ${status === "active" ? "badge--active badge--success" : "badge--inactive"}`}>{status === "active" ? "Active" : "Inactive"}</span>
-					<Link href={`/participants/${id}`} className="campaign-participant-count">
-						<a>
-							{/* <img src={iconParticipants} alt={`${campaignName} - Participants`} /> */}
+					<span className={`campaign-status badge ${status === "active" ? "badge--active badge--success" : "badge--inactive"}`}>{status === "active" ? "active" : "inactive"}</span>
+					<Link href={`/participants/${id}`}>
+						<a className="campaign-participant-count">
+							<span>
+								<Image src="/images/icons/icon-participants.svg" width={15} height={15} alt={`${name} - Participants`} />
+							</span>
 							Participants: <strong>{participants}</strong>
 						</a>
 					</Link>
@@ -37,7 +80,11 @@ export default function CampaignCard({ id, name, type, status, participants, vis
 					</p>
 				</div>
 			</div>
-			<div className="campaign-card__footer">Footer</div>
+			<div className="campaign-card__footer campaign-card__section">
+				<Link href={`/campaign/edit/${id}`} title={`Edit ${name}`}>
+					<a className="button button--outline">Edit Campaign</a>
+				</Link>
+			</div>
 		</div>
 	);
 }
--- components/dashboard-components/campaign-components/CampaignList.js
+++ components/dashboard-components/campaign-components/CampaignList.js
@@ -4,6 +4,8 @@ export default function CampaignList({ limit = 5 }) {
 	return (
 		<div id="campaign-list">
 			<CampaignCard id={12345} name={`This is a sample campaign`} type={`quiz`} status={`active`} participants={1234} visibleFrom={`2021.01.01.`} visibleTo={`2021.02.01.`} />
+			<CampaignCard id={12345} name={`This is a sample campaign`} type={`quiz`} status={`active`} participants={1234} visibleFrom={`2021.01.01.`} visibleTo={`2021.02.01.`} />
+			<CampaignCard id={12345} name={`This is a sample campaign`} type={`quiz`} status={`active`} participants={1234} visibleFrom={`2021.01.01.`} visibleTo={`2021.02.01.`} />
 		</div>
 	);
 }
