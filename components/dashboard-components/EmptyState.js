132207165313a7bbc74b82b034fac5ceafb4ed80
Ferenc Forgacs <forgacsf@gmail.com>
2021-01-06 11:37:07 +0100

    changed primary action from button to link

--- components/dashboard-components/EmptyState.js
+++ components/dashboard-components/EmptyState.js
@@ -1,14 +1,16 @@
-export default function EmptyStateCampaigns({ title = "Please, provide a title", description = "Please, provide a description", action = null, actionLabel = "", helpURL = "", helpLabel = "Learn more", illustration = "" }) {
+import Link from "next/link";
+
+export default function EmptyStateCampaigns({ title = "Please, provide a title", description = "Please, provide a description", actionLink = "", actionLabel = "", helpURL = "", helpLabel = "Learn more", illustration = "" }) {
 	return (
 		<div className="empty-state">
 			<div className="empty-state__body">
 				<h4>{title}</h4>
 
 				<p className="empty-state__copy">{description}</p>
-				{action && (
-					<button className="button button--primary button--autowidth" onClick={(e) => action()}>
-						{actionLabel}
-					</button>
+				{actionLink && (
+					<Link href={actionLink}>
+						<a className="button button--primary button--autowidth">{actionLabel}</a>
+					</Link>
 				)}
 				{helpURL && (
 					<div className="empty-state__help">
