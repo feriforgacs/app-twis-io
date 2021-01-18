35353ab63ad611a0c0dbc603254ca998ef7bebb8
Ferenc Forgacs <forgacsf@gmail.com>
2021-01-18 18:20:11 +0100

    editor sidebar ui changes

--- components/editor-components/Sidebar.js
+++ components/editor-components/Sidebar.js
@@ -1,13 +1,16 @@
+import { useState } from "react";
 import styles from "./Sidebar.module.scss";
 import Element from "./sidebar-components/Element";
 
 export default function Sidebar() {
+	const [activeElement, setActiveElement] = useState("text");
+
 	return (
 		<div id="editor__sidebar" className={styles.sidebar}>
 			<div id="sidebar__elements" className={styles.sidebarElements}>
-				<Element icon="text" label="Text" />
-				<Element icon="image" label="Image" />
-				<Element icon="sticker" label="Sticker" />
+				<Element icon="text" label="Text" active={activeElement === "text"} onClick={() => setActiveElement("text")} />
+				<Element icon="image" label="Image" active={activeElement === "image"} onClick={() => setActiveElement("image")} />
+				<Element icon="sticker" label="Sticker" active={activeElement === "sticker"} onClick={() => setActiveElement("sticker")} />
 			</div>
 			<div id="sidebar__element-options" className={styles.sidebarElementOptions}>
 				element settings
--- components/editor-components/sidebar-components/Element.js
+++ components/editor-components/sidebar-components/Element.js
@@ -1,9 +1,9 @@
 import styles from "./Element.module.scss";
 import Image from "next/image";
 
-export default function Element({ active = false, icon, label }) {
+export default function Element({ active = false, icon, label, onClick }) {
 	return (
-		<div className={`${styles.element} ${active ? styles.elementActive : ""}`}>
+		<div className={`${styles.element} ${active ? styles.elementActive : ""}`} onClick={onClick}>
 			<span className={styles.icon}>
 				<Image src={`/images/editor/icons/icon-${icon}.svg`} alt={`${label} icon`} width={20} height={20} />
 			</span>
