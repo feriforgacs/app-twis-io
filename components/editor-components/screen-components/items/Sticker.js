483c193a9c8322aa5b2d5ac6297db19005fdda94
Ferenc Forgacs <forgacsf@gmail.com>
2021-01-27 17:30:08 +0100

    sticker item screen component

--- components/editor-components/screen-components/items/Sticker.js
+++ components/editor-components/screen-components/items/Sticker.js
@@ -1,3 +1,10 @@
-export default function Sticker() {
-	return <div>I am a sticker</div>;
+export default function Sticker({ data, screenItemIndex }) {
+	const imageStyle = {
+		height: data.settings.height,
+		width: data.settings.width,
+		top: data.settings.top,
+		left: data.settings.left,
+		position: "absolute",
+	};
+	return <img src={data.src} style={imageStyle} />;
 }
