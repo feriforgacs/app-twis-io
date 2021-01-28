50d83864e7232a5780ad6c518df30994bc1d9321
Ferenc Forgacs <forgacsf@gmail.com>
2021-01-28 17:24:38 +0100

    unset active screen on click outside

--- components/editor-components/Body.js
+++ components/editor-components/Body.js
@@ -1,9 +1,20 @@
+import { useContext } from "react";
+import { GlobalContext } from "../../context/GlobalState";
 import styles from "./Body.module.scss";
 import ScreenList from "./screen-components/ScreenList";
 
 export default function Body() {
+	const { resetActiveScreen } = useContext(GlobalContext);
 	return (
-		<div id="editor__body" className={styles.body}>
+		<div
+			id="editor__body"
+			className={styles.body}
+			onMouseDown={(e) => {
+				if (e.target && e.target.id === "editor__body") {
+					resetActiveScreen();
+				}
+			}}
+		>
 			<ScreenList />
 		</div>
 	);
