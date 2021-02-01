43ab1d4d163f039c49fbc786ea73202ce2320059
Ferenc Forgacs <forgacsf@gmail.com>
2021-02-01 15:08:21 +0100

    header action buttons

--- components/editor-components/Header.js
+++ components/editor-components/Header.js
@@ -40,12 +40,6 @@ export default function Header() {
 					</Link>
 				</div>
 
-				<Link href="/dashboard">
-					<a>Dashboard</a>
-				</Link>
-
-				<span className={styles.separator}>&#8725;</span>
-
 				<Link href="/campaigns/">
 					<a>Campaigns</a>
 				</Link>
@@ -67,6 +61,25 @@ export default function Header() {
 						</svg>
 					</span>
 				</form>
+
+				<button className={styles.buttonCampaignSettings}>
+					Campaign Settings{" "}
+					<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+						<circle cx="12" cy="12" r="1"></circle>
+						<circle cx="19" cy="12" r="1"></circle>
+						<circle cx="5" cy="12" r="1"></circle>
+					</svg>
+				</button>
+				<button className={styles.buttonShare}>
+					Share{" "}
+					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+						<circle cx="18" cy="5" r="3"></circle>
+						<circle cx="6" cy="12" r="3"></circle>
+						<circle cx="18" cy="19" r="3"></circle>
+						<line x1="8.59" y1="13.51" x2="15.42" y2="17.49"></line>
+						<line x1="15.41" y1="6.51" x2="8.59" y2="10.49"></line>
+					</svg>
+				</button>
 			</div>
 		</>
 	);
