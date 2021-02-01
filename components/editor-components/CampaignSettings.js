3cd38b656919b5d5e4033bd4efff4b0171009001
Ferenc Forgacs <forgacsf@gmail.com>
2021-02-01 17:46:03 +0100

    update campaign visiblity dates

--- components/editor-components/CampaignSettings.js
+++ components/editor-components/CampaignSettings.js
@@ -1,11 +1,32 @@
 import { useContext, useState } from "react";
-import { GlobalContext } from "../../context/GlobalState";
 import Switch from "react-switch";
+import { GlobalContext } from "../../context/GlobalState";
 import styles from "./CampaignSettings.module.scss";
+import DayPickerInput from "react-day-picker/DayPickerInput";
+import { DateUtils } from "react-day-picker";
+import dateFnsFormat from "date-fns/format";
+import dateFnsParse from "date-fns/parse";
+import "react-day-picker/lib/style.css";
 
 export default function CampaignSettings() {
 	const { campaign, updateCampaignData } = useContext(GlobalContext);
 	const [active, setActive] = useState(campaign.status === "active" || false);
+	const [visibleFrom, setVisibleFrom] = useState(new Date(campaign.visibleFrom) || new Date());
+	const [visibleTo, setVisibleTo] = useState(new Date(campaign.visibleTo) || new Date());
+
+	const dateFormat = "yyyy.MM.dd.";
+
+	const parseDate = (str, format, locale) => {
+		const parsed = dateFnsParse(str, format, new Date(), { locale });
+		if (DateUtils.isDate(parsed)) {
+			return parsed;
+		}
+		return undefined;
+	};
+
+	const formatDate = (date, format, locale) => {
+		return dateFnsFormat(date, format, { locale });
+	};
 	return (
 		<div className={styles.campaignSettingsPanel}>
 			<div className={styles.settingsPanelSection}>
@@ -19,11 +40,44 @@ export default function CampaignSettings() {
 						offColor="#34495b"
 						onColor="#159c5b"
 					/>
-					<span>Campaign Active</span>
+					<span>
+						Status: <strong>{campaign.status === "active" ? "Active" : "Inactive"}</strong>
+					</span>
 				</label>
 				<p className={styles.settingsPanelHelp}>
 					Your campaign is <strong>{campaign.status === "active" ? "active" : "inactive"}</strong>. You can change the status by clicking on the toggle above.
 				</p>
+			</div>
+
+			<div className={styles.settingsPanelSection}>
+				<label className={styles.settingsPanelLabel}>Campaign Visible From</label>
+				<DayPickerInput
+					format={dateFormat}
+					parseDate={parseDate}
+					value={dateFnsFormat(new Date(visibleFrom), dateFormat)}
+					dayPickerProps={{ disabledDays: { before: new Date() }, firstDayOfWeek: 1 }}
+					onDayChange={(day) => {
+						setVisibleFrom(day);
+						updateCampaignData("visibleFrom", day);
+					}}
+				/>
+				<p className={styles.settingsPanelHelp}>From this date, the visitors of the campaign can submit their answers to the questions.</p>
+			</div>
+
+			<div className={styles.settingsPanelSection}>
+				<label className={styles.settingsPanelLabel}>Campaign Visible To</label>
+				<DayPickerInput
+					formatDate={formatDate}
+					format={dateFormat}
+					parseDate={parseDate}
+					value={dateFnsFormat(new Date(visibleTo), dateFormat)}
+					dayPickerProps={{ disabledDays: { before: new Date(visibleFrom) }, firstDayOfWeek: 1 }}
+					onDayChange={(day) => {
+						setVisibleTo(day);
+						updateCampaignData("visibleTo", day);
+					}}
+				/>
+				<p className={styles.settingsPanelHelp}>This is the last date when your players can’t submit their answers.</p>
 			</div>
 		</div>
 	);
