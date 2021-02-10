06866a8d0895fc5239a35dd1dd350a0c86b198d2
Ferenc Forgacs <forgacsf@gmail.com>
2021-02-10 21:17:38 +0100

    delete item with backspace or del button

--- components/editor-components/screen-components/ItemAdditionalActions.js
+++ components/editor-components/screen-components/ItemAdditionalActions.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useCallback, useEffect } from "react";
 import { GlobalContext } from "../../../context/GlobalState";
 import ReactTooltip from "react-tooltip";
 import Image from "next/image";
@@ -6,6 +6,42 @@ import styles from "./ItemAdditionalActions.module.scss";
 
 export default function ItemAdditionalActions() {
 	const { activeScreenItem, activeScreen, removeScreenItem } = useContext(GlobalContext);
+
+	/**
+	 * Handle keydown events to delete screen item on backspace and delete
+	 * @param {obj} event Keydown event object
+	 */
+	const handleKeyDown = useCallback(
+		(event) => {
+			// dont delete item if delete event happened in text input
+			if (event.target && event.target.type && (event.target.type === "text" || event.target.type === "textarea")) {
+				return;
+			}
+
+			if (event.keyCode && (event.keyCode === 8 || event.keyCode === 46)) {
+				if (event.target.isContentEditable) {
+					return;
+				}
+				event.preventDefault();
+				// delete active screen item
+
+				if (activeScreenItem.removeable !== false) {
+					removeScreenItem(activeScreen.screenId, activeScreenItem.itemId);
+				} else {
+					alert("This item cannot be removed");
+				}
+			}
+		},
+		[activeScreenItem, removeScreenItem, activeScreen]
+	);
+
+	useEffect(() => {
+		document.addEventListener("keydown", handleKeyDown, true);
+		return () => {
+			document.removeEventListener("keydown", handleKeyDown, true);
+		};
+	}, [handleKeyDown]);
+
 	return (
 		<>
 			<div className={styles.additionalActions}>
