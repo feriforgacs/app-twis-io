327bbe60f61dbd7efebea7dbeda846c24d0b3a7d
Ferenc Forgacs <forgacsf@gmail.com>
2021-02-11 20:59:36 +0100

    gradient background picker basics

--- components/editor-components/screen-components/screen-settings/Background.js
+++ components/editor-components/screen-components/screen-settings/Background.js
@@ -1,11 +1,15 @@
 import { useContext, useState, useEffect, useRef } from "react";
 import { SketchPicker } from "react-color";
+import { parse as GradientParser } from "gradient-parser";
 import { GlobalContext } from "../../../../context/GlobalState";
 import styles from "../ScreenSettings.module.scss";
 
 export default function Background() {
 	const { activeScreen, setActiveScreen, updateScreen, updateScreenInState } = useContext(GlobalContext);
 	const [background, setBackground] = useState(activeScreen.background.color || "#ffffff");
+	const [backgroundType, setBackgroundType] = useState(activeScreen.background.type || "solid");
+	const [gradientStartColor, setGradientStartColor] = useState("#ffffff");
+	const [gradientEndColor, setGradientEndColor] = useState("#eeeeee");
 	const [colorPickerVisible, setColorPickerVisible] = useState(false);
 
 	const colorPickerContainerRef = useRef();
@@ -18,6 +22,16 @@ export default function Background() {
 
 	useEffect(() => {
 		setBackground(activeScreen.background.color);
+
+		if (activeScreen.background.color !== "solid") {
+			const gradientSettings = GradientParser("linear-gradient(135.57deg, rgb(164, 38, 184) 0%, rgb(78, 156, 239) 93.45%)");
+
+			setGradientStartColor(gradientSettings[0].colorStops[0]);
+			setGradientEndColor(gradientSettings[0].colorStops[1]);
+		} else {
+			setGradientStartColor(activeScreen.background.color);
+			setGradientEndColor(activeScreen.background.color);
+		}
 	}, [activeScreen]);
 
 	useEffect(() => {
@@ -38,6 +52,25 @@ export default function Background() {
 			<button className={styles.colorPickerButton} onClick={() => setColorPickerVisible(true)} style={{ background }}></button>
 			{colorPickerVisible && (
 				<div className={styles.colorPickerContainer} ref={colorPickerContainerRef}>
+					<div className={styles.backgroundTypeTabs}>
+						<button className={`${styles.backgroundTypeTab} ${backgroundType === "solid" ? styles.backgroundTypeTabActive : ""}`} onClick={() => setBackgroundType("solid")}>
+							Solid Color
+						</button>
+
+						<button className={`${styles.backgroundTypeTab} ${backgroundType === "gradient" ? styles.backgroundTypeTabActive : ""}`} onClick={() => setBackgroundType("gradient")}>
+							Linear Gradient
+						</button>
+					</div>
+
+					<div className={styles.gradientColors} style={{ background }}>
+						<button className={styles.gradientColorsButton} style={{ background: gradientStartColor }}>
+							start color
+						</button>
+						<button className={styles.gradientColorsButton} style={{ background: gradientEndColor }}>
+							end color
+						</button>
+					</div>
+
 					<SketchPicker
 						disableAlpha={true}
 						className="colorpicker"
