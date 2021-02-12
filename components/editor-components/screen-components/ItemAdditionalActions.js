a457f156b75243c4aa98ae2f12e00afa3fd9f39f
Ferenc Forgacs <forgacsf@gmail.com>
2021-02-12 18:27:53 +0100

    item position icons and component basics

--- components/editor-components/screen-components/ItemAdditionalActions.js
+++ components/editor-components/screen-components/ItemAdditionalActions.js
@@ -1,9 +1,11 @@
 import styles from "./ItemAdditionalActions.module.scss";
+import Position from "./item-actions/Position";
 import Delete from "./item-actions/Delete";
 
 export default function ItemAdditionalActions() {
 	return (
 		<div className={styles.additionalActions}>
+			<Position />
 			<Delete />
 		</div>
 	);
---
+++ components/editor-components/screen-components/item-actions/Position.js
@@ -0,0 +1,29 @@
+import { useContext, useCallback, useEffect } from "react";
+import { GlobalContext } from "../../../../context/GlobalState";
+import ReactTooltip from "react-tooltip";
+import Image from "next/image";
+import styles from "../ItemAdditionalActions.module.scss";
+
+export default function Position() {
+	const { activeScreenItem, activeScreen } = useContext(GlobalContext);
+	return (
+		<>
+			<button data-for="itemAction" data-tip="Bring to front" className={`${styles.button} ${styles.buttonPosition}`} onClick={() => alert("to front")}>
+				<Image src="/images/editor/icons/icon-to-front.svg" width={18} height={18} alt="To front icon" title="To front" />
+			</button>
+
+			<button data-for="itemAction" data-tip="Bring forward" className={`${styles.button} ${styles.buttonPosition}`} onClick={() => alert("forward")}>
+				<Image src="/images/editor/icons/icon-forward.svg" width={18} height={18} alt="Forward icon" title="Forward" />
+			</button>
+
+			<button data-for="itemAction" data-tip="Send backward" className={`${styles.button} ${styles.buttonPosition}`} onClick={() => alert("backward")}>
+				<Image src="/images/editor/icons/icon-backward.svg" width={18} height={18} alt="Backward icon" title="Backward" />
+			</button>
+
+			<button data-for="itemAction" data-tip="Send to back" className={`${styles.button} ${styles.buttonPosition}`} onClick={() => alert("To back")}>
+				<Image src="/images/editor/icons/icon-to-back.svg" width={18} height={18} alt="To back icon" title="To back" />
+			</button>
+			<ReactTooltip id="itemAction" place="bottom" type="dark" effect="solid" getContent={(dataTip) => `${dataTip}`} />
+		</>
+	);
+}
