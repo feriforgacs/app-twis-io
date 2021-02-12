f72f0fab2144b38b1ca5dfe04806eb31db4c5a6d
Ferenc Forgacs <forgacsf@gmail.com>
2021-02-12 10:06:50 +0100

    revert

--- components/editor-components/screen-components/screen-settings/Background.js
+++ components/editor-components/screen-components/screen-settings/Background.js
@@ -1,6 +1,5 @@
 import { useContext, useState, useEffect, useRef } from "react";
 import { SketchPicker } from "react-color";
-import { parse as GradientParser } from "gradient-parser";
 import { GlobalContext } from "../../../../context/GlobalState";
 import styles from "../ScreenSettings.module.scss";
 
@@ -8,10 +7,6 @@ export default function Background() {
 	const { activeScreen, setActiveScreen, updateScreen, updateScreenInState } = useContext(GlobalContext);
 	const [background, setBackground] = useState(activeScreen.background.color || "#ffffff");
 	const [backgroundType, setBackgroundType] = useState(activeScreen.background.type || "solid");
-
-	const [gradientColorPicker, setGradientColorPicker] = useState("start");
-	const [gradientStartColor, setGradientStartColor] = useState("#ffffff");
-	const [gradientEndColor, setGradientEndColor] = useState("#eeeeee");
 
 	const [colorPickerVisible, setColorPickerVisible] = useState(false);
 
@@ -25,17 +20,6 @@ export default function Background() {
 
 	useEffect(() => {
 		setBackground(activeScreen.background.color);
-
-		if (activeScreen.background.color !== "solid") {
-			const gradientSettings = GradientParser("linear-gradient(135.57deg, rgb(164, 38, 184) 0%, rgb(78, 156, 239) 93.45%)");
-
-			setGradientStartColor(`rgb(${gradientSettings[0].colorStops[0].value[0]}, ${gradientSettings[0].colorStops[0].value[1]}, ${gradientSettings[0].colorStops[0].value[2]})`);
-
-			setGradientEndColor(`rgb(${gradientSettings[0].colorStops[1].value[0]}, ${gradientSettings[0].colorStops[1].value[1]}, ${gradientSettings[0].colorStops[1].value[2]})`);
-		} else {
-			setGradientStartColor(activeScreen.background.color);
-			setGradientEndColor(activeScreen.background.color);
-		}
 	}, [activeScreen]);
 
 	useEffect(() => {
@@ -82,57 +66,17 @@ export default function Background() {
 						</button>
 					</div>
 
-					{backgroundType === "gradient" && (
-						<div className={styles.gradientColors} style={{ background }}>
-							<button className={`${styles.gradientColorsButton} ${gradientColorPicker === "start" ? styles.gradientColorsButtonActive : ""}`} style={{ background: gradientStartColor }} onClick={() => setGradientColorPicker("start")}>
-								start
-							</button>
-
-							<button className={`${styles.gradientColorsButton} ${gradientColorPicker === "end" ? styles.gradientColorsButtonActive : ""}`} style={{ background: gradientEndColor }} onClick={() => setGradientColorPicker("end")}>
-								end
-							</button>
-						</div>
-					)}
-
 					<SketchPicker
 						disableAlpha={true}
 						className="colorpicker"
-						color={backgroundType === "solid" ? background : gradientColorPicker === "start" ? gradientStartColor : gradientEndColor}
+						color={background}
 						onChange={(color) => {
-							let backgroundColor;
-							if (backgroundType === "solid") {
-								backgroundColor = color.hex;
-							} else {
-								if (gradientColorPicker === "start") {
-									// set gradient start color
-									setGradientStartColor(`rgb(${color.rgb.r}, ${color.rgb.g}, ${color.rgb.b})`);
-									backgroundColor = `linear-gradient(rgb(${color.rgb}), ${gradientEndColor})`;
-								} else {
-									// set gradient end color
-									setGradientEndColor(`rgb(${color.rgb.r}, ${color.rgb.g}, ${color.rgb.b})`);
-									backgroundColor = `linear-gradient(${gradientStartColor}, rgb(${color.rgb}))`;
-								}
-							}
-							setBackground(backgroundColor);
-							updateScreenInState(activeScreen.screenId, { background: { type: backgroundType, color: backgroundColor } });
+							setBackground(color.hex);
+							updateScreenInState(activeScreen.screenId, { background: { type: backgroundType, color: color.hex } });
 						}}
 						onChangeComplete={(color) => {
-							let backgroundColor;
-							if (backgroundType === "solid") {
-								backgroundColor = color.hex;
-							} else {
-								if (gradientColorPicker === "start") {
-									// set gradient start color
-									setGradientStartColor(`rgb(${color.rgb.r}, ${color.rgb.g}, ${color.rgb.b})`);
-									backgroundColor = `linear-gradient(rgb(${color.rgb}), ${gradientEndColor})`;
-								} else {
-									// set gradient end color
-									setGradientEndColor(`rgb(${color.rgb.r}, ${color.rgb.g}, ${color.rgb.b})`);
-									backgroundColor = `linear-gradient(${gradientStartColor}, rgb(${color.rgb}))`;
-								}
-							}
-							updateScreen(activeScreen.screenId, { background: { type: backgroundType, color: backgroundColor } });
-							setActiveScreen({ ...activeScreen, background: { type: backgroundType, color: backgroundColor } });
+							updateScreen(activeScreen.screenId, { background: { type: backgroundType, color: color.hex } });
+							setActiveScreen({ ...activeScreen, background: { type: backgroundType, color: color.hex } });
 						}}
 					/>
 				</div>
