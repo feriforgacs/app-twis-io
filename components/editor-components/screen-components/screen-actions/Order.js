a249ce0489df2ec2569a7747ee91c4bd88b9f28c
Ferenc Forgacs <forgacsf@gmail.com>
2021-02-16 11:33:19 +0100

    disable move buttons for first and last screen items

--- components/editor-components/screen-components/screen-actions/Order.js
+++ components/editor-components/screen-components/screen-actions/Order.js
@@ -5,14 +5,14 @@ import Image from "next/image";
 import { GlobalContext } from "../../../../context/GlobalState";
 
 export default function Order({ screen }) {
-	const { updateScreenOrder } = useContext(GlobalContext);
+	const { screens, updateScreenOrder } = useContext(GlobalContext);
 	return (
 		<>
-			<button data-for="screenAction" data-tip="Move up" className={`${styles.buttonScreen} ${styles.buttonScreenMoveUp}`} onClick={() => updateScreenOrder(screen.screenId, "up")}>
+			<button data-for="screenAction" data-tip="Move up" disabled={screen.orderIndex === 1} className={`${styles.buttonScreen} ${styles.buttonScreenMoveUp}`} onClick={() => updateScreenOrder(screen.screenId, "up")}>
 				<Image src="/images/editor/icons/icon-move-up.svg" width={18} height={18} alt="Move screen up icon" title="Move up" />
 			</button>
 
-			<button data-for="screenAction" data-tip="Move down" className={`${styles.buttonScreen} ${styles.buttonScreenMoveDown}`} onClick={() => updateScreenOrder(screen.screenId, "down")}>
+			<button data-for="screenAction" data-tip="Move down" disabled={screen.orderIndex === screens.length - 3} className={`${styles.buttonScreen} ${styles.buttonScreenMoveDown}`} onClick={() => updateScreenOrder(screen.screenId, "down")}>
 				<Image src="/images/editor/icons/icon-move-down.svg" width={18} height={18} alt="Move screen down icon" title="Move down" />
 			</button>
 			<ReactTooltip id="screenAction" place="bottom" type="dark" effect="solid" getContent={(dataTip) => `${dataTip}`} />
