5d713324f7f37442bd48e65e62e48aca51e49faa
Ferenc Forgacs <forgacsf@gmail.com>
2021-02-23 21:13:14 +0100

    answers screen item basics

--- components/editor-components/screen-components/items/Answers.js
+++ components/editor-components/screen-components/items/Answers.js
@@ -1,3 +1,41 @@
-export default function Answers() {
-	return <div>Answers</div>;
+import { useContext } from "react";
+import { GlobalContext } from "../../../../context/GlobalState";
+import styles from "./Answers.module.scss";
+
+export default function Answers({ data }) {
+	const { activeScreenItem, setActiveScreenItem } = useContext(GlobalContext);
+	const screenItemActive = activeScreenItem.itemId === data.itemId;
+
+	let answersStyle = {
+		height: `${data.settings.height || 0}px`,
+		width: `${data.settings.width || 0}px`,
+		top: `${data.settings.top || 0}px`,
+		left: `${data.settings.left || 0}px`,
+		transform: `translateX(${data.settings.translateX || 0}px) translateY(${data.settings.translateY || 0}px) rotate(${data.settings.rotate || 0}deg)`,
+		textAlign: data.settings.align,
+		fontSize: `${data.settings.fontSize}px`,
+		fontWeight: data.settings.bold ? 700 : 400,
+		fontStyle: data.settings.italic ? `italic` : `normal`,
+		textDecoration: data.settings.underline ? `underline` : `none`,
+		textTransform: data.settings.uppercase ? `uppercase` : `none`,
+		opacity: typeof data.settings.opacity !== undefined ? data.settings.opacity : 1,
+		zIndex: data.orderIndex,
+		position: "absolute",
+	};
+
+	let answerItemStyle = {
+		background: data.settings.highlightColor.backgroundColor,
+		color: `rgba(${data.settings.color.r}, ${data.settings.color.g}, ${data.settings.color.b}, ${data.settings.color.a})`,
+	};
+
+	if (screenItemActive) {
+		answersStyle.cursor = "move";
+	}
+	return (
+		<>
+			<div onClick={() => setActiveScreenItem(data)} id={`${data.type}-${data.itemId}`} className={`screen-item ${styles.answers}`} style={answersStyle}>
+				<div className={styles.answerItem}>Answer item</div>
+			</div>
+		</>
+	);
 }
