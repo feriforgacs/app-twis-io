068d5147bca4fa18f8500e8f781eb6783a07de36
Ferenc Forgacs <forgacsf@gmail.com>
2021-03-01 17:51:53 +0100

    refactoring - moved campaign visibility settings to separate component

--- components/editor-components/CampaignSettings.js
+++ components/editor-components/CampaignSettings.js
@@ -1,10 +1,5 @@
 import { useContext, useState, useEffect, useRef } from "react";
-import Switch from "react-switch";
 import { GlobalContext } from "../../context/GlobalState";
-import DayPickerInput from "react-day-picker/DayPickerInput";
-import { DateUtils } from "react-day-picker";
-import dateFnsFormat from "date-fns/format";
-import dateFnsParse from "date-fns/parse";
 import { DebounceInput } from "react-debounce-input";
 import axios from "axios";
 import Button from "./sidebar-components/Button";
@@ -12,12 +7,11 @@ import ImageUploadPreview from "./sidebar-components/image-components/ImageUploa
 import Toast from "../dashboard-components/Toast";
 import "react-day-picker/lib/style.css";
 import styles from "./CampaignSettings.module.scss";
+import Visibility from "./campaign-settings-components/Visibility";
 
 export default function CampaignSettings({ hideCampaignSettings }) {
 	const { campaign, updateCampaignData, updateCampaignDataInState, screens } = useContext(GlobalContext);
-	const [active, setActive] = useState(campaign.status === "active" || false);
-	const [visibleFrom, setVisibleFrom] = useState(new Date(campaign.visibleFrom) || new Date());
-	const [visibleTo, setVisibleTo] = useState(new Date(campaign.visibleTo) || new Date());
+
 	const [uploading, setUploading] = useState(false);
 	const [deleting, setDeleting] = useState(false);
 	const [requestCancelToken, setRequestCancelToken] = useState();
@@ -49,20 +43,6 @@ export default function CampaignSettings({ hideCampaignSettings }) {
 			document.removeEventListener("click", handleClickOutside);
 		};
 	});
-
-	const dateFormat = "yyyy.MM.dd.";
-
-	const parseDate = (str, format, locale) => {
-		const parsed = dateFnsParse(str, format, new Date(), { locale });
-		if (DateUtils.isDate(parsed)) {
-			return parsed;
-		}
-		return undefined;
-	};
-
-	const formatDate = (date, format, locale) => {
-		return dateFnsFormat(date, format, { locale });
-	};
 
 	/**
 	 * Read selected image as data url to upload
@@ -200,63 +180,7 @@ export default function CampaignSettings({ hideCampaignSettings }) {
 
 	return (
 		<div className={styles.campaignSettingsPanel} ref={campaignSettingsRef}>
-			{/* Campaign Status */}
-			<div className={styles.settingsPanelSection}>
-				<label className={styles.settingsPanelLabel}>
-					<Switch
-						onChange={() => {
-							setActive(!active);
-							updateCampaignData("status", campaign.status === "active" ? "draft" : "active");
-						}}
-						checked={active}
-						onColor="#159c5b"
-					/>
-					<span>
-						Status: <strong>{campaign.status === "active" ? "Active" : "Inactive"}</strong>
-					</span>
-				</label>
-				<p className={styles.settingsPanelHelp}>
-					Your campaign is <strong>{campaign.status === "active" ? "active" : "inactive"}</strong>. You can change the status by clicking on the toggle above.
-				</p>
-			</div>
-
-			{/* Campaign Visiblity Dates */}
-			{active && (
-				<>
-					<div className={styles.settingsPanelSection}>
-						<label className={styles.settingsPanelLabel}>Campaign Visible From</label>
-						<DayPickerInput
-							format={dateFormat}
-							parseDate={parseDate}
-							value={dateFnsFormat(new Date(visibleFrom), dateFormat)}
-							inputProps={{ readOnly: true }}
-							dayPickerProps={{ disabledDays: { before: new Date() }, firstDayOfWeek: 1 }}
-							onDayChange={(day) => {
-								setVisibleFrom(day);
-								updateCampaignData("visibleFrom", day);
-							}}
-						/>
-						<p className={styles.settingsPanelHelp}>From this date, the visitors of the campaign can submit their answers to the questions.</p>
-					</div>
-
-					<div className={styles.settingsPanelSection}>
-						<label className={styles.settingsPanelLabel}>Campaign Visible To</label>
-						<DayPickerInput
-							formatDate={formatDate}
-							format={dateFormat}
-							parseDate={parseDate}
-							value={dateFnsFormat(new Date(visibleTo), dateFormat)}
-							inputProps={{ readOnly: true }}
-							dayPickerProps={{ disabledDays: { before: new Date(visibleFrom) }, firstDayOfWeek: 1 }}
-							onDayChange={(day) => {
-								setVisibleTo(day);
-								updateCampaignData("visibleTo", day);
-							}}
-						/>
-						<p className={styles.settingsPanelHelp}>This is the last date when your players can submit their answers.</p>
-					</div>
-				</>
-			)}
+			<Visibility />
 
 			{/* Success limit */}
 			<div className={styles.settingsPanelSection}>
---
+++ components/editor-components/campaign-settings-components/Visibility.js
@@ -0,0 +1,93 @@
+import { useContext, useState } from "react";
+import Switch from "react-switch";
+import DayPickerInput from "react-day-picker/DayPickerInput";
+import { DateUtils } from "react-day-picker";
+import dateFnsFormat from "date-fns/format";
+import dateFnsParse from "date-fns/parse";
+import { GlobalContext } from "../../../context/GlobalState";
+import "react-day-picker/lib/style.css";
+import styles from "../CampaignSettings.module.scss";
+
+export default function Visibility() {
+	const { campaign, updateCampaignData } = useContext(GlobalContext);
+
+	const [active, setActive] = useState(campaign.status === "active" || false);
+	const [visibleFrom, setVisibleFrom] = useState(new Date(campaign.visibleFrom) || new Date());
+	const [visibleTo, setVisibleTo] = useState(new Date(campaign.visibleTo) || new Date());
+
+	const dateFormat = "yyyy.MM.dd.";
+
+	const parseDate = (str, format, locale) => {
+		const parsed = dateFnsParse(str, format, new Date(), { locale });
+		if (DateUtils.isDate(parsed)) {
+			return parsed;
+		}
+		return undefined;
+	};
+
+	const formatDate = (date, format, locale) => {
+		return dateFnsFormat(date, format, { locale });
+	};
+
+	return (
+		<>
+			{/* Campaign Status */}
+			<div className={styles.settingsPanelSection}>
+				<label className={styles.settingsPanelLabel}>
+					<Switch
+						onChange={() => {
+							setActive(!active);
+							updateCampaignData("status", campaign.status === "active" ? "draft" : "active");
+						}}
+						checked={active}
+						onColor="#159c5b"
+					/>
+					<span>
+						Status: <strong>{campaign.status === "active" ? "Active" : "Inactive"}</strong>
+					</span>
+				</label>
+				<p className={styles.settingsPanelHelp}>
+					Your campaign is <strong>{campaign.status === "active" ? "active" : "inactive"}</strong>. You can change the status by clicking on the toggle above.
+				</p>
+			</div>
+
+			{/* Campaign Visiblity Dates */}
+			{active && (
+				<>
+					<div className={styles.settingsPanelSection}>
+						<label className={styles.settingsPanelLabel}>Campaign Visible From</label>
+						<DayPickerInput
+							format={dateFormat}
+							parseDate={parseDate}
+							value={dateFnsFormat(new Date(visibleFrom), dateFormat)}
+							inputProps={{ readOnly: true }}
+							dayPickerProps={{ disabledDays: { before: new Date() }, firstDayOfWeek: 1 }}
+							onDayChange={(day) => {
+								setVisibleFrom(day);
+								updateCampaignData("visibleFrom", day);
+							}}
+						/>
+						<p className={styles.settingsPanelHelp}>From this date, the visitors of the campaign can submit their answers to the questions.</p>
+					</div>
+
+					<div className={styles.settingsPanelSection}>
+						<label className={styles.settingsPanelLabel}>Campaign Visible To</label>
+						<DayPickerInput
+							formatDate={formatDate}
+							format={dateFormat}
+							parseDate={parseDate}
+							value={dateFnsFormat(new Date(visibleTo), dateFormat)}
+							inputProps={{ readOnly: true }}
+							dayPickerProps={{ disabledDays: { before: new Date(visibleFrom) }, firstDayOfWeek: 1 }}
+							onDayChange={(day) => {
+								setVisibleTo(day);
+								updateCampaignData("visibleTo", day);
+							}}
+						/>
+						<p className={styles.settingsPanelHelp}>This is the last date when your players can submit their answers.</p>
+					</div>
+				</>
+			)}
+		</>
+	);
+}
