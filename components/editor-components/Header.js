f4cada0042532c120a92f8b842e1c64d6cb8e51a
Ferenc Forgacs <forgacsf@gmail.com>
2021-03-01 21:34:44 +0100

    share buttons - UI

--- components/editor-components/Header.js
+++ components/editor-components/Header.js
@@ -7,13 +7,15 @@ import { DebounceInput } from "react-debounce-input";
 import { GlobalContext } from "../../context/GlobalState";
 import styles from "./Header.module.scss";
 import CampaignSettings from "./CampaignSettings";
+import SharePanel from "./SharePanel";
 import FontFamilies from "../../utils/FontFamilies";
 import SaveStatus from "./header-components/SaveStatus";
 
 export default function Header() {
 	const { loading, campaign, updateCampaignData } = useContext(GlobalContext);
 	const [name, setName] = useState(campaign.name || "loading...");
-	const [campsignSettingsVisible, toggleCampaignSettings] = useState(false);
+	const [campsignSettingsVisible, setCampaignSettingsVisible] = useState(false);
+	const [sharePanelVisible, setSharePanelVisible] = useState(false);
 	const [campaignFonts, setCampaignFonts] = useState(campaign.fonts || []);
 	const router = useRouter();
 
@@ -25,7 +27,11 @@ export default function Header() {
 	}, [campaign]);
 
 	const hideCampaignSettings = () => {
-		toggleCampaignSettings(false);
+		setCampaignSettingsVisible(false);
+	};
+
+	const hideSharePanel = () => {
+		setSharePanelVisible(false);
 	};
 
 	return (
@@ -66,8 +72,8 @@ export default function Header() {
 
 				<SaveStatus />
 
-				<button className={`${styles.buttonCampaignSettings} button--campaign-settings`} disabled={loading} onClick={() => toggleCampaignSettings(!campsignSettingsVisible)}>
-					Settings{" "}
+				<button className={`${styles.buttonCampaignSettings} button--campaign-settings`} disabled={loading} onClick={() => setCampaignSettingsVisible(!campsignSettingsVisible)}>
+					Campaign settings{" "}
 					{campsignSettingsVisible ? (
 						<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
 							<line x1="18" y1="6" x2="6" y2="18"></line>
@@ -81,15 +87,22 @@ export default function Header() {
 						</svg>
 					)}
 				</button>
-				<button className={styles.buttonShare} disabled={loading}>
+				<button className={styles.buttonShare} disabled={loading} onClick={() => setSharePanelVisible(!sharePanelVisible)}>
 					Share{" "}
-					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-						<circle cx="18" cy="5" r="3"></circle>
-						<circle cx="6" cy="12" r="3"></circle>
-						<circle cx="18" cy="19" r="3"></circle>
-						<line x1="8.59" y1="13.51" x2="15.42" y2="17.49"></line>
-						<line x1="15.41" y1="6.51" x2="8.59" y2="10.49"></line>
-					</svg>
+					{sharePanelVisible ? (
+						<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+							<line x1="18" y1="6" x2="6" y2="18"></line>
+							<line x1="6" y1="6" x2="18" y2="18"></line>
+						</svg>
+					) : (
+						<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#ffffff" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round">
+							<circle cx="18" cy="5" r="3"></circle>
+							<circle cx="6" cy="12" r="3"></circle>
+							<circle cx="18" cy="19" r="3"></circle>
+							<line x1="8.59" y1="13.51" x2="15.42" y2="17.49"></line>
+							<line x1="15.41" y1="6.51" x2="8.59" y2="10.49"></line>
+						</svg>
+					)}
 				</button>
 
 				<button className={styles.buttonPublish} disabled={loading}>
@@ -97,6 +110,7 @@ export default function Header() {
 				</button>
 			</div>
 
+			{sharePanelVisible && <SharePanel hideSharePanel={hideSharePanel} />}
 			{campsignSettingsVisible && <CampaignSettings hideCampaignSettings={hideCampaignSettings} />}
 		</>
 	);
