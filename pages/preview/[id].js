01c32ba17dd0670cdb83c202f0061f66e6d61a92
Ferenc Forgacs <forgacsf@gmail.com>
2021-03-02 21:57:44 +0100

    campaign od data

--- pages/preview/[id].js
+++ pages/preview/[id].js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
+import Head from "next/head";
 import NProgress from "nprogress";
+import FontFamilies from "../../utils/FontFamilies";
 
 export default function PreviewPage({ campaign, errorMessage }) {
 	const [loading, setLoading] = useState(true);
@@ -10,7 +12,25 @@ export default function PreviewPage({ campaign, errorMessage }) {
 	 * @todo display preview alert somewhere
 	 */
 
-	return <div>{errorMessage ? <p>{errorMessage}</p> : ""}Preview campaign</div>;
+	return (
+		<>
+			<Head>
+				<title>
+					Preview - {campaign.name} - {process.env.SITE_NAME}
+				</title>
+
+				{campaign.fonts.length > 0 && <link rel="preconnect" href="https://fonts.gstatic.com" />}
+				{campaign.fonts.map((font, index) => font !== "" && FontFamilies[font].url !== "" && <link key={index} href={FontFamilies[font].url} rel="stylesheet" />)}
+
+				<meta property="og:type" content="website" />
+				<meta property="og:url" content="TODO" />
+				{campaign.ogTitle && <meta property="og:title" content={campaign.ogTitle} />}
+				{campaign.ogDescription && <meta property="og:description" content={campaign.ogDescription} />}
+				{campaign.ogImage && <meta property="og:image" content={campaign.ogImage} />}
+			</Head>
+			<div>{errorMessage ? <p>{errorMessage}</p> : ""}Preview campaign</div>
+		</>
+	);
 }
 
 export async function getServerSideProps(context) {
