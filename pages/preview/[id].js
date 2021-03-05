dd37fd9e8b171dae78d8d089bd38acc44311cd47
Ferenc Forgacs <forgacsf@gmail.com>
2021-03-05 17:46:48 +0100

    preview - restart

--- pages/preview/[id].js
+++ pages/preview/[id].js
@@ -1,3 +1,10 @@
+/**
+ * @todo display first screen
+ * @todo display screens indicator
+ * @todo go to next screen
+ * @todo go to previous screen
+ * @todo calculate screen size
+ */
 import { useState, useEffect } from "react";
 import Head from "next/head";
 import FontFamilies from "../../utils/FontFamilies";
@@ -6,31 +13,15 @@ import ScreensIndicator from "../../components/frontend-components/ScreensIndica
 
 export default function PreviewPage({ campaign, errorMessage, screens }) {
 	const [activeScreenIndex, setActiveScreenIndex] = useState(0);
-	const campaignScreens = screens.slice(0, screens.length - 2);
-	const endScreens = screens.slice(screens.length - 2, screens.length);
-	const [endScreenData, setEndScreenData] = useState(endScreens[0]); // set success screen as default end screen
-	const lastScreenIndex = screens.length - 2;
-
-	const [screenWidth, setScreenWidht] = useState(360);
-	const [screenHeight, setScreenHeight] = useState(640);
-
 	/**
 	 * Go to the next screen
 	 */
-	const gotoNextScreen = () => {
-		if (activeScreenIndex < lastScreenIndex) {
-			setActiveScreenIndex(activeScreenIndex + 1);
-		}
-	};
+	const gotoNextScreen = () => {};
 
 	/**
 	 * Go to the previous screen
 	 */
-	const gotoPreviousScreen = () => {
-		if (activeScreenIndex > 0) {
-			setActiveScreenIndex(activeScreenIndex - 1);
-		}
-	};
+	const gotoPreviousScreen = () => {};
 
 	/**
 	 * Go to next or previous screen based on click location
@@ -51,8 +42,6 @@ export default function PreviewPage({ campaign, errorMessage, screens }) {
 		/**
 		 * @todo calculate screen width and height based on window size
 		 */
-		setScreenHeight(window.innerHeight);
-		setScreenWidht(window.innerWidth);
 	};
 
 	useEffect(() => {
@@ -61,11 +50,6 @@ export default function PreviewPage({ campaign, errorMessage, screens }) {
 			window.removeEventListener("resize", handleResize);
 		};
 	}, []);
-
-	/**
-	 * @todo display campaign
-	 * @todo display preview alert somewhere
-	 */
 
 	// display error message if there is one
 	if (errorMessage) return <p>{errorMessage}</p>;
@@ -89,18 +73,6 @@ export default function PreviewPage({ campaign, errorMessage, screens }) {
 
 			<div className="campaign" onClick={(e) => handleClick(e)}>
 				<div className="story">
-					<div
-						className="story__screens"
-						style={{
-							left: `-${activeScreenIndex * screenWidth}px`,
-							width: `${screens.length - 1 * screenWidth}px`,
-						}}
-					>
-						{campaignScreens.map((screen, index) => (
-							<CampaignScreen key={index} data={screen} width={screenWidth} height={screenHeight} />
-						))}
-						<CampaignScreen key="endscreen" data={endScreenData} width={screenWidth} height={screenHeight} />
-					</div>
 					<ScreensIndicator screens={screens.length - 1} active={activeScreenIndex} />
 				</div>
 			</div>
