056144e4e98f82bc4cc9515f255bad5f38f3bdd2
Ferenc Forgacs <forgacsf@gmail.com>
2021-03-09 21:42:16 +0100

    show selected answer - wip

--- components/frontend-components/screen-items/answer-items/AnswerOption.js
+++ components/frontend-components/screen-items/answer-items/AnswerOption.js
@@ -16,12 +16,31 @@ export default function AnswerOption({ index, answer, answerItemStyle, onClick, 
 		};
 	}
 
+	if (answered && !success) {
+		currentItemstyle = {
+			...answerItemStyle,
+			background: "#ff0000",
+			color: "#fff",
+		};
+	}
+
+	let successIconColor = "#ffffff";
+	if (answer.correct && !success) {
+		successIconColor = "#54ba00";
+	}
+
 	return (
 		<div className={styles.answerOption} style={currentItemstyle} onClick={onClick}>
-			<div className={styles.choice}>{answerChoices[index]}</div>
+			<div className={styles.choice}>
+				{answer.correct && answered && (
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke={successIconColor} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+						<polyline points="20 6 9 17 4 12"></polyline>
+					</svg>
+				)}
+				{!answer.correct && answered && <i>x</i>}
+				{!answered && answerChoices[index]}
+			</div>
 			<div className={styles.optionText}>{answer.option}</div>
-			{answer.correct && success ? "🟢" : "🛑"}
-			{answered ? "👍" : "👎"}
 		</div>
 	);
 }
