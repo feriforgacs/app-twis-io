037542b74e9188c2f4d82b58665c389482b5eeb8
Ferenc Forgacs <forgacsf@gmail.com>
2021-03-10 09:43:46 +0100

    display selected wrong answer

--- components/frontend-components/screen-items/answer-items/AnswerOption.js
+++ components/frontend-components/screen-items/answer-items/AnswerOption.js
@@ -16,7 +16,15 @@ export default function AnswerOption({ index, answer, answerItemStyle, onClick, 
 		};
 	}
 
-	let successIconColor = "#54ba00";
+	if (answer.option === selectedAnswer.option && !selectedAnswer.correct) {
+		currentItemstyle = {
+			...answerItemStyle,
+			background: "#f24e4e",
+			color: "#fff",
+		};
+	}
+
+	// choice icon background
 	const choiceBackground = selectedAnswer ? "#fff" : "transparent";
 
 	return (
@@ -28,11 +36,16 @@ export default function AnswerOption({ index, answer, answerItemStyle, onClick, 
 				}}
 			>
 				{answer.correct && answered && (
-					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke={successIconColor} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#54ba00" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
 						<polyline points="20 6 9 17 4 12"></polyline>
 					</svg>
 				)}
-				{!answer.correct && answered && <i>x</i>}
+				{!answer.correct && answered && (
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#f24e4e" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+						<line x1="18" y1="6" x2="6" y2="18"></line>
+						<line x1="6" y1="6" x2="18" y2="18"></line>
+					</svg>
+				)}
 				{!answered && answerChoices[index]}
 			</div>
 			<div className={styles.optionText}>{answer.option}</div>
