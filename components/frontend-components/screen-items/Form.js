b12169275c561c653b5659a93a9678918fd5f821
Ferenc Forgacs <forgacsf@gmail.com>
2021-03-11 21:37:39 +0100

    process form - wip

--- components/frontend-components/screen-items/Form.js
+++ components/frontend-components/screen-items/Form.js
@@ -1,6 +1,44 @@
+import { useState } from "react";
+
 import styles from "./Form.module.scss";
 
 export default function Form({ data }) {
+	const [name, setName] = useState("");
+	const [nameError, setNameError] = useState(false);
+
+	const [email, setEmail] = useState("");
+	const [emailError, setEmailError] = useState(false);
+
+	const [legalAccepted, setLegalAccepted] = useState(false);
+	const [legalError, setLegalError] = useState(false);
+
+	const [result, setResult] = useState("");
+
+	const processForm = () => {
+		let error = false;
+		if (data.settings.collectName && !name) {
+			setNameError(true);
+			error = true;
+		}
+
+		if (data.settings.collectEmail && !email) {
+			setEmailError(true);
+			error = true;
+		}
+
+		if (!legalAccepted) {
+			setLegalError(true);
+			error = true;
+		}
+
+		if (error) {
+			return;
+		}
+
+		// display success screen in preview mode
+		setResult("success");
+	};
+
 	let formStyle = {
 		background: data.settings.highlightColor.backgroundColor,
 		height: `${data.settings.height || 0}px`,
@@ -28,37 +66,76 @@ export default function Form({ data }) {
 	const fontFamilyClass = data.settings.fontFamily !== "" ? `font--${data.settings.fontFamily}` : `font--arial`;
 
 	return (
-		<div className={`no-step ${styles.form} ${fontFamilyClass}`} style={formStyle}>
-			<div className={`no-step ${styles.formBody}`}>
-				{data.settings.collectName && (
-					<div className={`no-step ${styles.formGroup}`}>
-						<label className={`no-step ${styles.formLabel}`}>{data.settings.labelName || "Name"}</label>
-						<input type="text" className={`no-step ${styles.formInput}`} />
+		<>
+			<div className={`no-step ${styles.form} ${fontFamilyClass}`} style={formStyle}>
+				<div className={`no-step ${styles.formBody}`}>
+					{data.settings.collectName && (
+						<div className={`no-step ${styles.formGroup} ${nameError ? styles.formGroupError : ""}`}>
+							<label className={`no-step ${styles.formLabel}`}>{data.settings.labelName || "Name"}</label>
+							<input
+								type="text"
+								value={name}
+								onChange={(e) => {
+									setName(e.target.value);
+									if (e.target.value !== "") {
+										setNameError(false);
+									} else {
+										setNameError(true);
+									}
+								}}
+								className={`no-step ${styles.formInput}`}
+							/>
+						</div>
+					)}
+
+					{data.settings.collectEmail && (
+						<div className={`no-step ${styles.formGroup} ${emailError ? styles.formGroupError : ""}`}>
+							<label className={`no-step ${styles.formLabel}`}>{data.settings.labelEmail || "Email address"}</label>
+							<input
+								type="email"
+								value={email}
+								onChange={(e) => {
+									setEmail(e.target.value);
+									if (e.target.value !== "") {
+										setEmailError(false);
+									} else {
+										setEmailError(true);
+									}
+								}}
+								className={`no-step ${styles.formInput}`}
+							/>
+						</div>
+					)}
+
+					<div className={`no-step ${styles.formGroup} ${legalError ? styles.formGroupError : ""}`}>
+						<label className={`no-step ${styles.labelLegal}`}>
+							<input
+								type="checkbox"
+								className={`no-step ${styles.legalCheckbox}`}
+								checked={legalAccepted}
+								onChange={(e) => {
+									setLegalAccepted(e.target.checked);
+									if (e.target.checked) {
+										setLegalError(false);
+									} else {
+										setLegalError(true);
+									}
+								}}
+							/>
+							<a href={data.settings.legalURL} target="_blank" rel="noopener noreferrer" style={{ color: formStyle.color }} className={`no-step `}>
+								{data.settings.labelTerms || "I've read and accept the terms & conditions"}
+							</a>
+						</label>
 					</div>
-				)}
 
-				{data.settings.collectEmail && (
 					<div className={`no-step ${styles.formGroup}`}>
-						<label className={`no-step ${styles.formLabel}`}>{data.settings.labelEmail || "Email address"}</label>
-						<input type="email" className={`no-step ${styles.formInput}`} />
+						<button className={`no-step ${styles.submitButton}`} style={buttonStyle} onClick={() => processForm()}>
+							{data.settings.labelSubmit || "Submit"}
+						</button>
 					</div>
-				)}
-
-				<div className={`no-step ${styles.formGroup}`}>
-					<label className={`no-step ${styles.labelLegal}`}>
-						<input type="checkbox" className={`no-step ${styles.legalCheckbox}`} />
-						<a href={data.settings.legalURL} target="_blank" rel="noopener noreferrer" style={{ color: formStyle.color }} className={`no-step `}>
-							{data.settings.labelTerms || "I've read and accept the terms & conditions"}
-						</a>
-					</label>
-				</div>
-
-				<div className={`no-step ${styles.formGroup}`}>
-					<button className={`no-step ${styles.submitButton}`} style={buttonStyle}>
-						{data.settings.labelSubmit || "Submit"}
-					</button>
 				</div>
 			</div>
-		</div>
+			{result && result === "success" && <p>success</p>}
+		</>
 	);
 }
