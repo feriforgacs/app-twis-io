b5d3886eeb67099dd4820d3687e3f4cf7ae01f75
Ferenc Forgacs <forgacsf@gmail.com>
2021-03-18 21:26:06 +0100

    hide preview, publish, settings buttons while loading

--- components/editor-components/Header.js
+++ components/editor-components/Header.js
@@ -72,37 +72,41 @@ export default function Header() {
 
 				<SaveStatus />
 
-				<a href={`/preview/${campaign._id}`} target="_blank" rel="noopener noreferrer" className={styles.buttonPreview}>
-					<span>Preview</span>
-					<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#fff" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round">
-						<g fill="none">
-							<path d="M18 14v5a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8c0-1.1.9-2 2-2h5M15 3h6v6M10 14L20.2 3.8" />
-						</g>
-					</svg>
-				</a>
-				<button
-					className={`${styles.buttonCampaignSettings} button--campaign-settings`}
-					disabled={loading}
-					onClick={() => {
-						setCampaignSettingsVisible(!campsignSettingsVisible);
-						unsetActiveScreenItem();
-						unsetActiveScreen();
-					}}
-				>
-					Campaign settings
-				</button>
+				{campaign._id && (
+					<>
+						<a href={`/preview/${campaign._id}`} target="_blank" rel="noopener noreferrer" className={styles.buttonPreview}>
+							<span>Preview</span>
+							<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#fff" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round">
+								<g fill="none">
+									<path d="M18 14v5a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8c0-1.1.9-2 2-2h5M15 3h6v6M10 14L20.2 3.8" />
+								</g>
+							</svg>
+						</a>
+						<button
+							className={`${styles.buttonCampaignSettings} button--campaign-settings`}
+							disabled={loading}
+							onClick={() => {
+								setCampaignSettingsVisible(!campsignSettingsVisible);
+								unsetActiveScreenItem();
+								unsetActiveScreen();
+							}}
+						>
+							Campaign settings
+						</button>
 
-				<button
-					className={styles.buttonPublish}
-					disabled={loading}
-					onClick={() => {
-						setPublishPanelVisible(!publishPanelVisible);
-						unsetActiveScreenItem();
-						unsetActiveScreen();
-					}}
-				>
-					Publish
-				</button>
+						<button
+							className={styles.buttonPublish}
+							disabled={loading}
+							onClick={() => {
+								setPublishPanelVisible(!publishPanelVisible);
+								unsetActiveScreenItem();
+								unsetActiveScreen();
+							}}
+						>
+							Publish
+						</button>
+					</>
+				)}
 			</div>
 
 			{campsignSettingsVisible && <CampaignSettings hideCampaignSettings={hideCampaignSettings} />}
