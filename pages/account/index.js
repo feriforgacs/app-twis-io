2c68bc1f4d8167e386644ac51761fb8ee8799ceb
Ferenc Forgacs <forgacsf@gmail.com>
2021-03-26 20:02:05 +0100

    [wip] account page

--- pages/account/index.js
+++ pages/account/index.js
@@ -21,6 +21,41 @@ export default function AccountPage() {
 			<Sidebar />
 			<div id="page__content">
 				<PageHeader title="Account" />
+
+				<div>
+					<h3>Subscription</h3>
+					<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis, autem.</p>
+
+					<div className="subscription__options">
+						<div className="subscription-option">
+							<h3>Basic</h3>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, dicta.</p>
+							<button>Upgrade to this plan</button>
+							<button>Downgrade to this plan</button>
+						</div>
+
+						<div className="subscription-option">
+							<h3>Pro</h3>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, dicta.</p>
+							<button>Upgrade to this plan</button>
+							<button>Downgrade to this plan</button>
+						</div>
+
+						<div className="subscription-option">
+							<h3>Premium</h3>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, dicta.</p>
+							<button>Upgrade to this plan</button>
+							<button>Downgrade to this plan</button>
+						</div>
+					</div>
+
+					<div>
+						<h4>Cancel subscription</h4>
+						<p>You can cancel your subscription any time by clicking the button below. The collected participant information and the campaigns you created won&apos;t be affected.</p>
+						<p>If your current subscription is not the best option for you, you can also dowgrade your account to a smaller plan.</p>
+						<button>Cancel subscription</button>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
