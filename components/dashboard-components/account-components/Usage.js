ce95ddaf0c6b35d6800e7923f5372dfb3c21a3b5
Ferenc Forgacs <forgacsf@gmail.com>
2021-04-03 11:07:15 +0200

    usage tooltip

--- components/dashboard-components/account-components/Usage.js
+++ components/dashboard-components/account-components/Usage.js
@@ -2,13 +2,17 @@
  * @todo get usage from the db
  * @todo display usage numbers and dates in tooltip
  */
+import ReactTooltip from "react-tooltip";
+
 export default function Usage() {
 	return (
-		<div className="sidebar__usage-status">
+		<div className="sidebar__usage-status" data-for="usagetooltip" data-tip="With your current plan, you can collect 34 more unique participants until 2021.04.05.">
 			<div className="usage-status__progress-bar">
 				<div className="usage-status__progress-value"></div>
 			</div>
 			<span>50% left of your monthly usage</span>
+
+			<ReactTooltip id="usagetooltip" place="top" type="dark" effect="solid" multiline={true} getContent={(dataTip) => dataTip} />
 		</div>
 	);
 }
