717fd1593b4ad81a56b70f6f9959ac7b54f52642
Ferenc Forgacs <forgacsf@gmail.com>
2021-04-07 21:32:47 +0200

    [wip] plan management - refactoring

--- components/dashboard-components/account-components/Subscription.js
+++ components/dashboard-components/account-components/Subscription.js
@@ -1,19 +1,10 @@
-import { useState, useEffect } from "react";
-import { format } from "date-fns";
-import Toast from "../Toast";
+import { useState } from "react";
 import Refund from "./Refund";
+import SubscriptionStatus from "./SubscriptionStatus";
 import SubscriptionPlans from "./SubscriptionPlans";
 import SubscriptionCancel from "./SubscriptionCancel";
 
 export default function Subscription() {
-	const [loading, setLoading] = useState(false);
-	const [currentUsage, setCurrentUsage] = useState({ limit: 0, value: 0, renewDate: Date.now() });
-
-	const [toastMessage, setToastMessage] = useState(false);
-	const [toastVisible, setToastVisible] = useState(false);
-	const [toastType, setToastType] = useState("default");
-	const [toastDuration, setToastDuration] = useState(3000);
-
 	const [currentPlan, setCurrentPlan] = useState(""); // @todo set based on user current plan
 	const [currentPlanTerm, setCurrentPlanTerm] = useState("monthly"); // @todo set based on user current plan term
 	const [planTerm, setPlanTerm] = useState("yearly"); // @todo set based on user current plan term
@@ -54,85 +45,17 @@ export default function Subscription() {
 		setCurrentPlanTerm(planTerm);
 	};
 
-	useEffect(() => {
-		const getUsage = async () => {
-			try {
-				const usageDataRequest = await fetch(`/api/account/usage`, {
-					method: "GET",
-				});
-
-				const usageData = await usageDataRequest.json();
-
-				setLoading(false);
-
-				if (usageData.success !== true) {
-					// error
-					setLoading(false);
-					// error
-					setToastMessage("Can't get usage information.");
-					setToastType("error");
-					setToastDuration(6000);
-					setToastVisible(true);
-					return;
-				}
-
-				if (usageData.data) {
-					setCurrentUsage(usageData.data);
-				}
-			} catch (error) {
-				console.log(error);
-				setLoading(false);
-				// error
-				setToastMessage("Can't get usage information.");
-				setToastType("error");
-				setToastDuration(6000);
-				setToastVisible(true);
-			}
-
-			return;
-		};
-
-		getUsage();
-	}, []);
-
 	return (
 		<div>
 			<h3 className="section-title">Subscription</h3>
 
-			{loading ? (
-				"loading..."
-			) : (
-				<div className="user-subscription">
-					<div className="user-subscription__plan">
-						<p>
-							Your current plan: <strong>{currentPlan ? `${plans[currentPlan].name} - ${currentPlanTerm}` : "You are not subscribed to any of the plans at the moment"}</strong>
-						</p>
-					</div>
-					<div className="user-subscription__usage">
-						<p>
-							Usage limit: <strong>{currentPlan ? `` : `You can collect 10 unique participants until ${format(new Date(currentUsage.renewDate), "yyy.MM.dd.")}`}</strong>
-						</p>
-						<p>
-							Usage status: <strong>{currentPlan ? `` : `${currentUsage.value} / 10`}</strong>
-						</p>
-						{currentPlan && (
-							<p>
-								Plan renew date: <strong>{format(new Date(currentUsage.renewDate), "yyy.MM.dd.")}</strong>
-							</p>
-						)}
-						{currentPlan && currentUsage.value > currentUsage.limit && <p>Overages cost: TODO</p>}
-					</div>
-					<div className="user-subscription__renew-date"></div>
-				</div>
-			)}
+			<SubscriptionStatus currentPlan={currentPlan} currentPlanTerm={currentPlanTerm} plans={plans} />
 
 			<SubscriptionPlans planTerm={planTerm} setPlanTerm={setPlanTerm} currentPlan={currentPlan} plans={plans} currentPlanTerm={currentPlanTerm} setSubscription={setSubscription} />
 
 			{currentPlan && <SubscriptionCancel currentPlan={currentPlan} />}
 
 			<Refund />
-
-			{toastVisible && <Toast onClose={() => setToastVisible(false)} duration={toastDuration} type={toastType} content={toastMessage} />}
 		</div>
 	);
 }
