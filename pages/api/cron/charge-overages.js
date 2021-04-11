4142dc9334286aa11ae6ee3fb42ba1a79bb578d0
Ferenc Forgacs <forgacsf@gmail.com>
2021-04-11 19:17:26 +0200

    [wip] chareg overages

--- pages/api/cron/charge-overages.js
+++ pages/api/cron/charge-overages.js
@@ -1,6 +1,8 @@
 import Cors from "cors";
 import initMiddleware from "../../../lib/InitMiddleware";
 import DatabaseConnect from "../../../lib/DatabaseConnect";
+import Usage from "../../../models/Usage";
+import Subscription from "../../../models/Subscription";
 
 const cors = initMiddleware(
 	Cors({
@@ -16,7 +18,18 @@ export default async function ChargeOveragesHandler(req, res) {
 	 * @todo reset overages on success
 	 */
 	await cors(req, res);
+	await DatabaseConnect();
+
+	// get extra usage from the database
 	try {
+		const overages = await Usage.find({ $expr: { $gt: ["value", "limit"] } }).and({ renewDate: { $lte: new Date(Date.now()) } });
+
+		if (overages.length <= 0) {
+			return res.status(200).send("no overages to charge today");
+		}
+
+		// get subscription for users
+		return res.status(200).json({ overages });
 	} catch (error) {
 		console.log(error);
 		return res.status(400).json({ success: false, error });
