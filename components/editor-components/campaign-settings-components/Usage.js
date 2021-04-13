c8eb3058e76621fc24e257bcd6687459e0ae7114
Ferenc Forgacs <forgacsf@gmail.com>
2021-04-13 21:35:07 +0200

    [wip] display current usage in publish panel

--- components/editor-components/campaign-settings-components/Usage.js
+++ components/editor-components/campaign-settings-components/Usage.js
@@ -1,25 +1,26 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { format } from "date-fns";
 import styles from "../CampaignSettings.module.scss";
 
 export default function Usage() {
 	const [usageLoading, setUsageLoading] = useState(true);
 	const [currentUsage, setCurrentUsage] = useState({ limit: 0, value: 0, renewDate: Date.now() });
 	const [currentUsageError, setCurrentUsageError] = useState("");
-	const [requestCancelToken, setRequestCancelToken] = useState();
 	const [usageLeft, setUsageLeft] = useState(0);
 
 	useEffect(() => {
 		/**
 		 * Get usage info for current user
 		 */
+		let requestCancelToken;
 		const getUsage = async () => {
 			if (requestCancelToken) {
 				requestCancelToken.cancel();
 			}
 
 			let source = axios.CancelToken.source();
-			setRequestCancelToken(source);
+			requestCancelToken = source;
 
 			try {
 				const usageResult = await axios.get(
@@ -40,9 +41,9 @@ export default function Usage() {
 					setCurrentUsage(usageResult.data.data);
 					let currentUsageLeft = 0;
 					if (usageResult.data.data.value === 0) {
-						currentUsageLeft = 100;
+						currentUsageLeft = usageResult.data.data.limit;
 					} else if (usageResult.data.data.value < usageResult.data.data.limit) {
-						currentUsageLeft = 100 - Math.floor((usageResult.data.data.value / usageResult.data.data.limit) * 100);
+						currentUsageLeft = usageResult.data.data.limit - usageResult.data.data.value;
 					}
 					setUsageLeft(currentUsageLeft);
 				}
@@ -57,12 +58,34 @@ export default function Usage() {
 		};
 
 		getUsage();
+
+		return () => {
+			if (requestCancelToken) {
+				requestCancelToken.cancel();
+			}
+		};
 	}, []);
 
 	return (
 		<div className={styles.settingsPanelSection}>
-			{usageLoading && <p>loading...</p>}
-			{!currentUsageError ? `Usage aaa ${usageLeft}` : ""}
+			{usageLoading ? (
+				"loading..."
+			) : (
+				<div>
+					{usageLeft && !currentUsageError ? (
+						<>
+							With your current plan, you can collect {usageLeft} more unique participants until {format(new Date(currentUsage.renewDate), "yyyy.MM.dd.")} without additional costs.
+						</>
+					) : (
+						<>You&apos;ve reached your monthly usage limit. It&apos;ll reset on {format(new Date(currentUsage.renewDate), "yyyy.MM.dd.")}</>
+					)}
+					Check detailed usage info under{" "}
+					<a href="/account" target="_blank" rel="noopener noreferrer">
+						your account
+					</a>
+					.
+				</div>
+			)}
 		</div>
 	);
 }
