e5b341a9668c894cd2aacfab4997d52722d7dfa0
Ferenc Forgacs <forgacsf@gmail.com>
2021-04-27 20:51:08 +0200

    [fix] save planTerm to db

--- models/Subscription.js
+++ models/Subscription.js
@@ -10,7 +10,7 @@ const subscriptionSchema = new mongoose.Schema({
 		type: String,
 		required: [true, "Subscription plan is required"],
 	},
-	panTerm: {
+	planTerm: {
 		type: String,
 		required: [true, "Subscription term is required"],
 	},
