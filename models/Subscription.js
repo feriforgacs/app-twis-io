117592894e2c9e5259b959ecead27c3277301f78
Ferenc Forgacs <forgacsf@gmail.com>
2021-05-05 22:03:14 +0200

    charge overages

--- models/Subscription.js
+++ models/Subscription.js
@@ -1,65 +1,86 @@
 import mongoose from "mongoose";
 
-const subscriptionSchema = new mongoose.Schema({
-	userId: {
-		type: mongoose.Schema.ObjectId,
-		ref: "User",
-		required: [true, "User id is required for subscription"],
+const subscriptionSchema = new mongoose.Schema(
+	{
+		userId: {
+			type: mongoose.Schema.ObjectId,
+			ref: "User",
+			required: [true, "User id is required for subscription"],
+		},
+		plan: {
+			type: String,
+			required: [true, "Subscription plan is required"],
+		},
+		planTerm: {
+			type: String,
+			required: [true, "Subscription term is required"],
+		},
+		customerId: {
+			type: String,
+			required: [true, "Customer ID is required"],
+		},
+		paymentDate: {
+			type: Date,
+		},
+		checkoutId: {
+			type: String,
+			required: [true, "Checkout ID is required"],
+		},
+		subscriptionId: {
+			type: Number,
+			required: [true, "Subscription ID is required"],
+		},
+		orderId: {
+			type: Number,
+			required: [true, "Subscription ID is required"],
+		},
+		productId: {
+			type: String,
+			required: [true, "Product ID is required"],
+		},
+		cancelUrl: {
+			type: String,
+		},
+		updateUrl: {
+			type: String,
+		},
+		status: {
+			type: String,
+		},
+		monthlyFee: {
+			type: Number,
+		},
+		overagesPrice: {
+			type: Number,
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now,
+		},
+		updatedAt: {
+			type: Date,
+			default: Date.now,
+		},
 	},
-	plan: {
-		type: String,
-		required: [true, "Subscription plan is required"],
-	},
-	planTerm: {
-		type: String,
-		required: [true, "Subscription term is required"],
-	},
-	customerId: {
-		type: String,
-		required: [true, "Customer ID is required"],
-	},
-	paymentDate: {
-		type: Date,
-	},
-	checkoutId: {
-		type: String,
-		required: [true, "Checkout ID is required"],
-	},
-	subscriptionId: {
-		type: Number,
-		required: [true, "Subscription ID is required"],
-	},
-	orderId: {
-		type: Number,
-		required: [true, "Subscription ID is required"],
-	},
-	productId: {
-		type: String,
-		required: [true, "Product ID is required"],
-	},
-	cancelUrl: {
-		type: String,
-	},
-	updateUrl: {
-		type: String,
-	},
-	status: {
-		type: String,
-	},
-	monthlyFee: {
-		type: Number,
-	},
-	overagesPrice: {
-		type: Number,
-	},
-	createdAt: {
-		type: Date,
-		default: Date.now,
-	},
-	updatedAt: {
-		type: Date,
-		default: Date.now,
-	},
+	{
+		toObject: { virtuals: true },
+		toJSON: { virtuals: true },
+	}
+);
+
+subscriptionSchema.virtual("usage", {
+	ref: "Usage",
+	localField: "userId",
+	foreignField: "userId",
+	justOne: true,
+});
+
+subscriptionSchema.pre("find", function () {
+	this.populate("usage");
+});
+
+subscriptionSchema.pre("findOne", function () {
+	this.populate("usage");
 });
 
 export default mongoose.models.Subscription || mongoose.model("Subscription", subscriptionSchema);
---
+++ pages/api/subscription/charge-overages.js
@@ -0,0 +1,82 @@
+import Cors from "cors";
+import axios from "axios";
+import { addMonths } from "date-fns";
+import initMiddleware from "../../../lib/InitMiddleware";
+import DatabaseConnect from "../../../lib/DatabaseConnect";
+import Usage from "../../../models/Usage";
+import Subscription from "../../../models/Subscription";
+
+const cors = initMiddleware(
+	Cors({
+		methods: ["GET"],
+	})
+);
+
+export default async function ChargeOveragesHandler(req, res) {
+	await cors(req, res);
+	await DatabaseConnect();
+
+	// get extra usage from the database
+	try {
+		let overages = await Usage.find({ $expr: { $gt: ["value", "limit"] } })
+			.and({ renewDate: { $lte: new Date(Date.now()) } })
+			.and({ trialAccount: false })
+			.distinct("userId");
+
+		if (overages.length <= 0) {
+			return res.status(200).send("no overages to charge today");
+		}
+
+		// get subscriptions
+		const subscriptions = await Subscription.find({ userId: { $in: overages }, status: "active" });
+
+		if (subscriptions.length <= 0) {
+			return res.status(200).send("no active subscriptions found for overages");
+		}
+
+		// calculate overages costs and initiate extra charge
+		for (const subscription of subscriptions) {
+			const overagesAmount = subscription.usage.value - subscription.usage.limit;
+			const overagesCost = subscription.overagesPrice * overagesAmount;
+			const renewDate = addMonths(new Date(Date.now()), 1);
+
+			// only charge overages above 5 dollars
+			if (overagesCost >= 5) {
+				const overagesChargeResult = await axios.post(
+					`${process.env.PADDLE_API_ENDPOINT}subscription/${subscription.subscriptionId}/charge`,
+					{
+						vendor_id: process.env.NEXT_PUBLIC_PADDLE_VENDOR_ID,
+						vendor_auth_code: process.env.PADDLE_AUTH_CODE,
+						amount: overagesCost,
+						charge_name: `twis.io - ${subscription.plan} plan monthly overages`,
+					},
+					{
+						headers: {
+							"Content-Type": "application/json",
+						},
+					}
+				);
+
+				if (!overagesChargeResult.data.success) {
+					/**
+					 * @todo send error msg to admin and user
+					 */
+				} else {
+					// reset usage
+					await Usage.findOneAndUpdate({ _id: subscription.usage._id }, { value: 0, renewDate });
+				}
+			} else {
+				// reduce usage only to the amount of overages
+				// if it was 110, reduce it to 10
+				// keep 10 so it can be charged in the next month
+				// @todo log errors
+				await Usage.findOneAndUpdate({ _id: subscription.usage._id }, { value: overagesAmount, renewDate });
+			}
+		}
+
+		return res.status(200).json({ success: true });
+	} catch (error) {
+		console.log(error);
+		return res.status(400).json({ success: false, error });
+	}
+}
