6019253913ed8e0d94b4c181a7e36f5f492e9201
Ferenc Forgacs <forgacsf@gmail.com>
2021-05-13 21:11:18 +0200

    🚧 moving preview to the frontend

--- components/editor-components/Header.js
+++ components/editor-components/Header.js
@@ -74,7 +74,7 @@ export default function Header() {
 
 				{campaign._id && (
 					<>
-						<a href={`/preview/${campaign._id}`} target="_blank" rel="noopener noreferrer" className={styles.buttonPreview}>
+						<a href={`${process.env.CAMPAIGN_URL_PREFIX}${campaign.url}?preview=true`} target="_blank" rel="noopener noreferrer" className={styles.buttonPreview}>
 							<span>Preview</span>
 							<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#fff" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round">
 								<g fill="none">
--- components/frontend-components/ScreensIndicator.js
+++
@@ -1,8 +0,0 @@
-export default function ScreensIndicator({ screens, active = 0 }) {
-	let indicators = [];
-	for (let i = 0; i < screens; i++) {
-		indicators.push(<div key={i} className={`indicator ${i <= active ? "indicator--active" : ""}`}></div>);
-	}
-
-	return <div className="screens-indicator">{indicators}</div>;
-}
