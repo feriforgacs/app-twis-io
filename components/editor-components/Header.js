5bc0151bd8e099299bf58762a571cfa1f8a5bebd
Ferenc Forgacs <forgacsf@gmail.com>
2021-08-06 17:39:32 +0200

    use id based preview urls

--- components/editor-components/Header.js
+++ components/editor-components/Header.js
@@ -74,7 +74,7 @@ export default function Header() {
 
 				{campaign._id && (
 					<>
-						<a href={`${process.env.CAMPAIGN_URL_PREFIX}${campaign.url}?preview=true`} target="_blank" rel="noopener noreferrer" className={styles.buttonPreview}>
+						<a href={`${process.env.CAMPAIGN_URL_PREFIX}${campaign._id}?preview=true`} target="_blank" rel="noopener noreferrer" className={styles.buttonPreview}>
 							<span>Preview</span>
 							<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#fff" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round">
 								<g fill="none">
